bccd3356f4561633128b893f15539c3a1946a04d
Christof Angermueller <cangermueller@gmail.com>
2016-06-18 04:08:03 +0100

    Update link to data.zip

--- examples/data.sh
+++ examples/data.sh
@@ -3,5 +3,5 @@
 # Downloads example data sets from Smallwood et al. (2014) including
 # input-output examples for 10000 CpG sites.
 
-wget https://www.dropbox.com/s/d8x0bl59nekucsb/data.zip?dl=0 -O data.zip
+wget https://www.dropbox.com/s/zncozl4hypzoaoo/data.zip?dl=0 -O data.zip
 unzip data.zip
